a77f9c90ac671b98ee7bcdee65dc43f6aa3fe8f2
sskeith1 <skeith1@babson.edu>
2017-09-11 15:42:41 -0400

    doneish?

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,12 +6,9 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
-
 	//Home page
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname, "../views/index.handlebars")
 	});
-
 
 }
