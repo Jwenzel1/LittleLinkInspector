3ba5ba4baf4db6eb969ce9ea5de4cf003511d1d6
gatan45 <joewenzel45@gmail.com>
2017-09-14 10:22:11 -0400

    added the extension

---
+++ Extension/content.js
@@ -0,0 +1,35 @@
+var url = "https://guarded-wave-66271.herokuapp.com/api/links/";
+$("a").hover(
+  function(event){ // Runs when the mouse enters an element
+    console.log($(this).attr("href").includes("bit.ly/"));
+    if($(this).attr("href").includes("bit.ly/")){
+      var $popupDiv = $("<div>");
+      $popupDiv.css({
+        "top": event.clientY + "px",
+        "left": event.clientX + "px"
+      });
+      $popupDiv.attr("id", "linkInspector");
+      $popupDiv.append($("<p>Getting Link Data</p>"));
+      $popupDiv.append($("<img>").attr("src", chrome.runtime.getURL("images/DontPanic.jpg")));
+      $("body").append($popupDiv);
+      $.get(url + encodeURIComponent($(this).attr("href")), function(response){
+        var longLink = $("<p>").text("Long URL: " + response.domain_name);
+        var safeText = $("<p>");
+        var malicious = response.malicious;
+        $popupDiv.text("");
+        $popupDiv.css("width", response.domain_name.length * 14 + 40 + "px");
+        $popupDiv.children().remove();
+        $popupDiv.append(longLink);
+        if(malicious){
+          $popupDiv.append(safeText.text("Malicious"));
+        }
+        else{
+          $popupDiv.append(safeText.text("Safe"));
+        }
+      });
+    }
+  },
+  function(){ // Runs when the mouse leaves an element
+    $("#linkInspector").remove();
+  }
+);
